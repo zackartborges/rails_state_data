5d1949f3291daef224115d6aa39f31e5e5f2b555
Zack Borges <zackartborges@gmail.com>
2021-06-19 13:21:24 -0400

    adds some csv to json logic (not sure if works), adds some views for states

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -1,5 +1,13 @@
 class Api::StatesController < ApplicationController
+# require 'csv'
+# require 'json'
+  def csv_to_json
+    csv_file_path = 'csv_files/state_data.csv'
+    csv = CSV.parse(csv_file_path).to_json
+
+  end
   def index
-    
+    @states = csv
+    render "index.json.jb"
   end
 end
---
+++ app/models/state.rb
@@ -0,0 +1,6 @@
+class State < ApplicationRecord
+  # require 'csv'
+  # require 'json'
+
+  # CSV.parse()
+end
