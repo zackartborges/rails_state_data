13c1154d8200ab438ff1649f0f23aa8b0a1fb1d4
Zack Borges <zackartborges@gmail.com>
2021-06-20 10:06:19 -0400

    adds csv gem

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -1,13 +1,8 @@
 class Api::StatesController < ApplicationController
 # require 'csv'
 # require 'json'
-  def csv_to_json
-    csv_file_path = 'csv_files/state_data.csv'
-    csv = CSV.parse(csv_file_path).to_json
 
-  end
   def index
-    @state = csv
-    render "index.json.jb"
+    
   end
 end
