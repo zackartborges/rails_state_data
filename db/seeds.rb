deac807905aed986c857c5ee4edffb065ddc837b
Zack Borges <zackartborges@gmail.com>
2021-06-20 16:32:37 -0400

    adds csv copy required att and fake seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+state = State.create([{name: "Texas", median_household_income: "392", share_unemployed_seasonal: "22", share_population_in_metro_areas: "2222", share_population_with_high_school_degree: "98"}])
