5804c8847af786ab8b578f52ecfe50de54b65b21
Zack Borges <zackartborges@gmail.com>
2021-06-20 17:33:30 -0400

    gets views in order, deletes csvtoJson logic

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -3,18 +3,8 @@ require 'csv'
 # require 'json'
 
   def index
-    @states = []
-    CSV.foreach(Rails.root.join('lib/state_data.csv'), skip_lines: /^#/, headers: true) do |row|
-    state = {
-      state: row[0],
-      median_household_income: row[1],
-      share_unemployed_seasonal: row[2],
-      share_population_in_metro_areas: row[3],
-      share_population_with_high_school_degree: row[4]
-    }
-    @states << state
-  end
-  render json: @states
+@state = State.all
+render "index.json.jb"
   end
 
   def show
