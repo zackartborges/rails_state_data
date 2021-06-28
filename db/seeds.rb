b5360ce2faf1189aa9b23823ec902b8da3acb4ea
Zack Borges <zackartborges@gmail.com>
2021-06-27 20:43:26 -0400

    adds abbrev and code to views

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,32 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # state = State.create([{name: "Texas", median_household_income: "392", share_unemployed_seasonal: "22", share_population_in_metro_areas: "2222", share_population_with_high_school_degree: "98"}])
-require 'csv'
+# require 'csv'
 
 # csv_text = File.read(Rails.root.join('lib', 'seeds', 'abbreviations.csv'))
 
 # puts csv_text
 
-index = 1
-CSV.foreach("lib/abbreviations.csv") do |row|
+# index = 1
+# CSV.foreach("lib/abbreviations.csv") do |row|
 #  need to bypass first row for the rows to match
 # first row is titles, need to bypass first row and match State.find(index)
   # p row[0]
-  if row[0] == State.find(index)["state"]
-    p row[2]
-    p State.find(index)["state"]
-    p "-------"
-    @state = State.find(index)
-    @state.code = row[2]
-    @state.save
-    index += 1
-  else
-    p "next"
-  end
+  # if row[0] == State.find(index)["state"]
+  #   p row[2]
+  #   p State.find(index)["state"]
+  #   p "-------"
+  #   @state = State.find(index)
+  #   @state.code = row[2]
+  #   @state.save
+  #   index += 1
+  # else
+  #   p "next"
+  # end
   
   # p State.find(index)["state"]
   # index = index + 1
   # p "----------"
 
   
-end
+# end
