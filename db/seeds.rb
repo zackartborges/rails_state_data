f466e7ec897b7e6e94694d3f04f5105536c7f7fe
Zack Borges <zackartborges@gmail.com>
2021-06-27 20:39:16 -0400

    successfully populates abbrev using seed file --wooHOO

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,30 @@
 # state = State.create([{name: "Texas", median_household_income: "392", share_unemployed_seasonal: "22", share_population_in_metro_areas: "2222", share_population_with_high_school_degree: "98"}])
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'abbreviations.csv'))
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'abbreviations.csv'))
 
-puts csv_text
+# puts csv_text
+
+index = 1
+CSV.foreach("lib/abbreviations.csv") do |row|
+#  need to bypass first row for the rows to match
+# first row is titles, need to bypass first row and match State.find(index)
+  # p row[0]
+  if row[0] == State.find(index)["state"]
+    p row[1]
+    p State.find(index)["state"]
+    p "-------"
+    @state = State.find(index)
+    @state.abbrev = row[1]
+    @state.save
+    index += 1
+  else
+    p "next"
+  end
+  
+  # p State.find(index)["state"]
+  # index = index + 1
+  # p "----------"
+
+  
+end
