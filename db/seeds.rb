8cdf5d51fd527ebd0e9d25ca798ca5e058c635cc
Zack Borges <zackartborges@gmail.com>
2021-08-09 09:38:49 -0400

    adds seed dump to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# state = State.create([{name: "Texas", median_household_income: "392", share_unemployed_seasonal: "22", share_population_in_metro_areas: "2222", share_population_with_high_school_degree: "98"}])
-# require 'csv'
-
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'abbreviations.csv'))
-
-# puts csv_text
-index = 1
-CSV.foreach("lib/abbreviations.csv") do |row|
-#  need to bypass first row for the rows to match
-# first row is titles, need to bypass first row and match State.find(index)
-  # p row[0]
-  if row[0] == State.find(index)["state"]
-    p row[1]
-    p State.find(index)["state"]
-    p "-------"
-    @state = State.find(index)
-    @state.code = row[2]
-    @state.save
-    index += 1
-  else
-    p "next"
-  end
-
-  # p State.find(index)["state"]
-  # index = index + 1
-  # p "----------"
-
-
-end 
+State.create!([
+  {state: "Michigan", median_household_income: "52005", share_unemployed_seasonal: "0.05", share_population_in_metro_areas: "0.87", share_population_with_high_school_degree: "0.879", abbrev: "Mich.", code: "MI"},
+  {state: "Minnesota", median_household_income: "67244", share_unemployed_seasonal: "0.038", share_population_in_metro_areas: "0.75", share_population_with_high_school_degree: "0.915", abbrev: "Minn.", code: "MN"},
+  {state: "Mississippi", median_household_income: "35521", share_unemployed_seasonal: "0.061", share_population_in_metro_areas: "0.45", share_population_with_high_school_degree: "0.804", abbrev: "Miss.", code: "MS"},
+  {state: "Missouri", median_household_income: "56630", share_unemployed_seasonal: "0.053", share_population_in_metro_areas: "0.78", share_population_with_high_school_degree: "0.868", abbrev: "Mo.", code: "MO"},
+  {state: "Montana", median_household_income: "51102", share_unemployed_seasonal: "0.041", share_population_in_metro_areas: "0.34", share_population_with_high_school_degree: "0.908", abbrev: "Mont.", code: "MT"},
+  {state: "Nebraska", median_household_income: "56870", share_unemployed_seasonal: "0.029", share_population_in_metro_areas: "0.6", share_population_with_high_school_degree: "0.898", abbrev: "Nebr.", code: "NE"},
+  {state: "Nevada", median_household_income: "49875", share_unemployed_seasonal: "0.067", share_population_in_metro_areas: "0.87", share_population_with_high_school_degree: "0.839", abbrev: "Nev.", code: "NV"},
+  {state: "New Hampshire", median_household_income: "73397", share_unemployed_seasonal: "0.034", share_population_in_metro_areas: "0.63", share_population_with_high_school_degree: "0.913", abbrev: "N.H.", code: "NH"},
+  {state: "New Jersey", median_household_income: "65243", share_unemployed_seasonal: "0.056", share_population_in_metro_areas: "1", share_population_with_high_school_degree: "0.874", abbrev: "N.J.", code: "NJ"},
+  {state: "New Mexico", median_household_income: "46686", share_unemployed_seasonal: "0.068", share_population_in_metro_areas: "0.69", share_population_with_high_school_degree: "0.828", abbrev: "N.M.", code: "NM"},
+  {state: "New York", median_household_income: "54310", share_unemployed_seasonal: "0.051", share_population_in_metro_areas: "0.94", share_population_with_high_school_degree: "0.847", abbrev: "N.Y.", code: "NY"},
+  {state: "North Carolina", median_household_income: "46784", share_unemployed_seasonal: "0.058", share_population_in_metro_areas: "0.76", share_population_with_high_school_degree: "0.843", abbrev: "N.C.", code: "NC"},
+  {state: "North Dakota", median_household_income: "60730", share_unemployed_seasonal: "0.028", share_population_in_metro_areas: "0.5", share_population_with_high_school_degree: "0.901", abbrev: "N.D.", code: "ND"},
+  {state: "Ohio", median_household_income: "49644", share_unemployed_seasonal: "0.045", share_population_in_metro_areas: "0.75", share_population_with_high_school_degree: "0.876", abbrev: "Ohio", code: "OH"},
+  {state: "Oklahoma", median_household_income: "47199", share_unemployed_seasonal: "0.044", share_population_in_metro_areas: "0.59", share_population_with_high_school_degree: "0.856", abbrev: "Okla.", code: "OK"},
+  {state: "Oregon", median_household_income: "58875", share_unemployed_seasonal: "0.062", share_population_in_metro_areas: "0.87", share_population_with_high_school_degree: "0.891", abbrev: "Ore.", code: "OR"},
+  {state: "Pennsylvania", median_household_income: "55173", share_unemployed_seasonal: "0.053", share_population_in_metro_areas: "0.87", share_population_with_high_school_degree: "0.879", abbrev: "Pa.", code: "PA"},
+  {state: "Rhode Island", median_household_income: "58633", share_unemployed_seasonal: "0.054", share_population_in_metro_areas: "1", share_population_with_high_school_degree: "0.847", abbrev: "R.I.", code: "RI"},
+  {state: "South Carolina", median_household_income: "44929", share_unemployed_seasonal: "0.057", share_population_in_metro_areas: "0.79", share_population_with_high_school_degree: "0.836", abbrev: "S.C.", code: "SC"},
+  {state: "South Dakota", median_household_income: "53053", share_unemployed_seasonal: "0.035", share_population_in_metro_areas: "0.51", share_population_with_high_school_degree: "0.899", abbrev: "S.D.", code: "SD"},
+  {state: "Tennessee", median_household_income: "43716", share_unemployed_seasonal: "0.057", share_population_in_metro_areas: "0.82", share_population_with_high_school_degree: "0.831", abbrev: "Tenn.", code: "TN"},
+  {state: "Texas", median_household_income: "53875", share_unemployed_seasonal: "0.042", share_population_in_metro_areas: "0.92", share_population_with_high_school_degree: "0.799", abbrev: "Tex.", code: "TX"},
+  {state: "Alabama", median_household_income: "42278", share_unemployed_seasonal: "0.06", share_population_in_metro_areas: "0.64", share_population_with_high_school_degree: "0.821", abbrev: "Ala.", code: "AL"},
+  {state: "Alaska", median_household_income: "67629", share_unemployed_seasonal: "0.064", share_population_in_metro_areas: "0.63", share_population_with_high_school_degree: "0.914", abbrev: "Alaska", code: "AK"},
+  {state: "Arizona", median_household_income: "49254", share_unemployed_seasonal: "0.063", share_population_in_metro_areas: "0.9", share_population_with_high_school_degree: "0.842", abbrev: "Ariz.", code: "AZ"},
+  {state: "Arkansas", median_household_income: "44922", share_unemployed_seasonal: "0.052", share_population_in_metro_areas: "0.69", share_population_with_high_school_degree: "0.824", abbrev: "Ark.", code: "AR"},
+  {state: "California", median_household_income: "60487", share_unemployed_seasonal: "0.059", share_population_in_metro_areas: "0.97", share_population_with_high_school_degree: "0.806", abbrev: "Calif.", code: "CA"},
+  {state: "Colorado", median_household_income: "60940", share_unemployed_seasonal: "0.04", share_population_in_metro_areas: "0.8", share_population_with_high_school_degree: "0.893", abbrev: "Colo.", code: "CO"},
+  {state: "Connecticut", median_household_income: "70161", share_unemployed_seasonal: "0.052", share_population_in_metro_areas: "0.94", share_population_with_high_school_degree: "0.886", abbrev: "Conn.", code: "CT"},
+  {state: "Delaware", median_household_income: "57522", share_unemployed_seasonal: "0.049", share_population_in_metro_areas: "0.9", share_population_with_high_school_degree: "0.874", abbrev: "Del.", code: "DE"},
+  {state: "District of Columbia", median_household_income: "68277", share_unemployed_seasonal: "0.067", share_population_in_metro_areas: "1", share_population_with_high_school_degree: "0.871", abbrev: "D.C.", code: "DC"},
+  {state: "Florida", median_household_income: "46140", share_unemployed_seasonal: "0.052", share_population_in_metro_areas: "0.96", share_population_with_high_school_degree: "0.853", abbrev: "Fla.", code: "FL"},
+  {state: "Georgia", median_household_income: "49555", share_unemployed_seasonal: "0.058", share_population_in_metro_areas: "0.82", share_population_with_high_school_degree: "0.839", abbrev: "Ga.", code: "GA"},
+  {state: "Hawaii", median_household_income: "71223", share_unemployed_seasonal: "0.034", share_population_in_metro_areas: "0.76", share_population_with_high_school_degree: "0.904", abbrev: "Hawaii", code: "HI"},
+  {state: "Idaho", median_household_income: "53438", share_unemployed_seasonal: "0.042", share_population_in_metro_areas: "0.7", share_population_with_high_school_degree: "0.884", abbrev: "Idaho", code: "ID"},
+  {state: "Illinois", median_household_income: "54916", share_unemployed_seasonal: "0.054", share_population_in_metro_areas: "0.9", share_population_with_high_school_degree: "0.864", abbrev: "Ill.", code: "IL"},
+  {state: "Indiana", median_household_income: "48060", share_unemployed_seasonal: "0.044", share_population_in_metro_areas: "0.79", share_population_with_high_school_degree: "0.866", abbrev: "Ind.", code: "IN"},
+  {state: "Iowa", median_household_income: "57810", share_unemployed_seasonal: "0.036", share_population_in_metro_areas: "0.6", share_population_with_high_school_degree: "0.914", abbrev: "Iowa", code: "IA"},
+  {state: "Kansas", median_household_income: "53444", share_unemployed_seasonal: "0.044", share_population_in_metro_areas: "0.64", share_population_with_high_school_degree: "0.897", abbrev: "Kans.", code: "KS"},
+  {state: "Kentucky", median_household_income: "42786", share_unemployed_seasonal: "0.05", share_population_in_metro_areas: "0.56", share_population_with_high_school_degree: "0.817", abbrev: "Ky.", code: "KY"},
+  {state: "Louisiana", median_household_income: "42406", share_unemployed_seasonal: "0.06", share_population_in_metro_areas: "0.81", share_population_with_high_school_degree: "0.822", abbrev: "La.", code: "LA"},
+  {state: "Maine", median_household_income: "51710", share_unemployed_seasonal: "0.044", share_population_in_metro_areas: "0.54", share_population_with_high_school_degree: "0.902", abbrev: "Maine", code: "ME"},
+  {state: "Maryland", median_household_income: "76165", share_unemployed_seasonal: "0.051", share_population_in_metro_areas: "0.97", share_population_with_high_school_degree: "0.89", abbrev: "Md.", code: "MD"},
+  {state: "Massachusetts", median_household_income: "63151", share_unemployed_seasonal: "0.046", share_population_in_metro_areas: "0.97", share_population_with_high_school_degree: "0.89", abbrev: "Mass.", code: "MA"},
+  {state: "Utah", median_household_income: "63383", share_unemployed_seasonal: "0.036", share_population_in_metro_areas: "0.82", share_population_with_high_school_degree: "0.904", abbrev: "Utah", code: "UT"},
+  {state: "Vermont", median_household_income: "60708", share_unemployed_seasonal: "0.037", share_population_in_metro_areas: "0.35", share_population_with_high_school_degree: "0.91", abbrev: "Vt.", code: "VT"},
+  {state: "Virginia", median_household_income: "66155", share_unemployed_seasonal: "0.043", share_population_in_metro_areas: "0.89", share_population_with_high_school_degree: "0.866", abbrev: "Va.", code: "VA"},
+  {state: "Washington", median_household_income: "59068", share_unemployed_seasonal: "0.052", share_population_in_metro_areas: "0.86", share_population_with_high_school_degree: "0.897", abbrev: "Wash.", code: "WA"},
+  {state: "West Virginia", median_household_income: "39552", share_unemployed_seasonal: "0.073", share_population_in_metro_areas: "0.55", share_population_with_high_school_degree: "0.828", abbrev: "W.Va.", code: "WV"},
+  {state: "Wisconsin", median_household_income: "58080", share_unemployed_seasonal: "0.043", share_population_in_metro_areas: "0.69", share_population_with_high_school_degree: "0.898", abbrev: "Wis.", code: "WI"},
+  {state: "Wyoming", median_household_income: "55690", share_unemployed_seasonal: "0.04", share_population_in_metro_areas: "0.31", share_population_with_high_school_degree: "0.918", abbrev: "Wyo.", code: "WY"}
+])
